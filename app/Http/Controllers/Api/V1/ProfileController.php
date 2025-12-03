29190d07215e609e7c5d386fd3f82608f7e39947
workjaykanzariya-byte <work.jaykanzariya@gmail.com>
2025-12-03 21:30:54 +0530

    Fix profile show request to avoid validation

--- app/Http/Controllers/Api/V1/ProfileController.php
+++ app/Http/Controllers/Api/V1/ProfileController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\Profile\UpdateProfileRequest;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    public function show(UpdateProfileRequest $request): JsonResponse
+    public function show(Request $request): JsonResponse
     {
         $user = $request->user();
 
