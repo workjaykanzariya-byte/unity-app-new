01a55b02891d71f49703dd2a726d7798c8c42c05
workjaykanzariya-byte <work.jaykanzariya@gmail.com>
2025-12-29 17:06:06 +0530

    Restore web as default auth guard while keeping Sanctum guard

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
 'defaults' => [
-    'guard' => env('AUTH_GUARD', 'sanctum'),
+    'guard' => env('AUTH_GUARD', 'web'),
     'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
 ],
 
