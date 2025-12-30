c7f3a677e345c2c386e485d8158fb83c3e144ffe
workjaykanzariya-byte <work.jaykanzariya@gmail.com>
2025-12-30 13:35:14 +0530

    Fix admin main entry import path for Vite

--- resources/js/admin/main.tsx
+++ resources/js/admin/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import '../css/app.css';
 
 const container = document.getElementById('admin-root');
 
