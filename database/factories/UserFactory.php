807304f73a09958151c77aa6dc7954a3d789d94a
workjaykanzariya-byte <work.jaykanzariya@gmail.com>
2025-12-31 16:12:07 +0530

    Add Tan Hars seed and update user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Str;
 class UserFactory extends Factory
 {
     /**
-     * The current password being used by the factory.
+     * The current hashed password being used by the factory.
      */
     protected static ?string $password;
 
@@ -23,22 +23,22 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $firstName = $this->faker->firstName();
+        $lastName = $this->faker->lastName();
+        $displayName = trim($firstName . ' ' . $lastName);
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+            'display_name' => $displayName,
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->unique()->numerify('##########'),
+            'company_name' => $this->faker->company(),
+            'membership_status' => 'visitor',
+            'coins_balance' => 0,
+            'password_hash' => static::$password ??= Hash::make('password'),
+            'public_profile_slug' => Str::slug($displayName) . '-' . Str::random(6),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
-    }
 }
